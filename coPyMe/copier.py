55b671ca3993cd32f0a8404a6a5399dc2f97eb57
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-16 14:27:13 +0100

    Updated copier

--- coPyMe/copier.py
+++ coPyMe/copier.py
@@ -4,10 +4,20 @@ from shutil import copy
 
 path = '\\\\192.168.8.26\\Graphics\\POWER STOP\\WIDEN Drive Backup\\'
 
-with open('./kits.txt', 'r') as file:
+with open('./kits2.txt', 'r') as file:
     for row in file:
         image = row.split(',')[1]
         image = image[3:]
+        image = image.strip()
+        
+        if image.endswith(('XR')):
+            image = image.replace('XR', 'XPR')
+        if image.endswith('XL'):
+            continue
+        
+        image = image + '.jpg'
+
+        print(image)
         image = image.strip()
         for root, dirs, files in os.walk(path):
             for file in files:
@@ -17,4 +27,4 @@ with open('./kits.txt', 'r') as file:
                     my_path = os.path.join(os.getcwd(), my_path)
                     os.makedirs(my_path, exist_ok=True)
                     copy(image_path, my_path)
-                    print(my_path)
+                    print(image_path)
