3127d58c75625ce27dfbc3f2bee23134e94fa8a1
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-17 11:06:42 +0100

    Added working renamer script

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -1,17 +1,51 @@
+
+import requests
+from bs4 import BeautifulSoup
+import pprint
+import collections
+from os import walk, listdir
 import os
+from os.path import join, isfile
+import difflib
 
-path = 'your_path'
+mypath = 'F:\\courses\\Laracasts\\_temp' 
+url = 'https://laracasts.com/series/how-do-i'
+header_class = "episode-list-title tw-mb-1 link lg:tw-leading-none tw-font-bold tw-text-xl md:tw-text-lg lg:tw-text-xl lg:tw-mb-0 tw-flex tw-items-center tw-mr-3"
+div_class = "episode-list-details"
 
-os.chdir(path)
-files = filter(os.path.isfile, os.listdir(path))
-files = [os.path.join(path, f) for f in files] # add path to each file
-files.sort(key=lambda x: os.path.getctime(x))
+def start_scrape(url, class_name):
+    my_lists = []
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    #print(soup)
+    for i in soup.findAll('div', attrs={'class' : class_name}):
+        content = i.find('a').contents[0]
+        my_lists.append(content.strip())
+    return dict(zip(my_lists, range(1, len(my_lists) + 1)))
+   
+def start_renaming(pairs):
+    if not isinstance(pairs, dict):
+        raise TypeError
+    #for el in pairs:
+        #print(el, pairs[el])
+    
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    keepcharacters = (' ','.','_', '-')
 
-i = 1
-to_replace = 'Testing Jargon- '
-for file in files:
-    # print(file + ' ' + str(os.path.getmtime(file)))
-    old_name = file
-    new_name = old_name.replace(to_replace, str(i) + '-')
-    os.rename(old_name, new_name)
-    i += 1
+    for file in onlyfiles:
+        rename = difflib.get_close_matches(file, pairs.keys(), n = 1)[0]
+        if rename:
+            old_path = os.path.join(mypath, file)
+            extension = os.path.splitext(file)[1]
+            new_filename = str(pairs[rename]) + '-' + rename + extension
+            new_filename = ''.join(c for c in new_filename if c.isalnum() or c in keepcharacters).rstrip()
+            new_path = os.path.join(mypath, new_filename)
+            os.rename(old_path, new_path)
+            print("renamed with pos ", pairs[rename])
+
+
+mydict = start_scrape(url, div_class)
+sorted_dict = collections.OrderedDict(mydict)
+               
+ 
+start_renaming(mydict)
