2ea4c85e69789b480a13aae98e3d64914198d222
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2019-12-17 12:02:26 +0100

    Added move to a new folder feature

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -7,9 +7,14 @@ from os import walk, listdir
 import os
 from os.path import join, isfile
 import difflib
+import shutil
 
-mypath = '/your/path' 
-url = 'https://yoururl.domain/'
+mypath = 'your_path'
+mypath_new = 'your_path_for_new_folder' 
+url = 'your_url'
+name_to_replace = 'name to replace'
+new_folder_name = os.path.join(mypath_new, 'new folder name')
+
 header_class = "episode-list-title tw-mb-1 link lg:tw-leading-none tw-font-bold tw-text-xl md:tw-text-lg lg:tw-text-xl lg:tw-mb-0 tw-flex tw-items-center tw-mr-3"
 div_class = "episode-list-details"
 
@@ -30,8 +35,13 @@ def start_renaming(pairs):
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     keepcharacters = (' ','.','_', '-')
 
+    if not os.path.exists(new_folder_name):
+        os.mkdir(new_folder_name)
+
     for file in onlyfiles:
-        rename = difflib.get_close_matches(file, pairs.keys(), n = 1)[0]
+        search = file.replace(name_to_replace, '')
+        rename = difflib.get_close_matches(search, pairs.keys(), n = 1)[0]
+        print(pairs)
         if rename:
             old_path = os.path.join(mypath, file)
             extension = os.path.splitext(file)[1]
@@ -40,6 +50,7 @@ def start_renaming(pairs):
             new_path = os.path.join(mypath, new_filename)
             os.rename(old_path, new_path)
             print("renamed with pos ", pairs[rename])
+            shutil.move(new_path, new_folder_name)
 
 
 mydict = start_scrape(url, div_class)
